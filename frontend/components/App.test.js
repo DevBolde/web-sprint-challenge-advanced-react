6f23c1b7c6576677ff0b437b1dfb71057b18b99e
Devin Bolden <devinbolden6@gmail.com>
2023-04-20 12:57:34 -0500

    wip

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,17 @@
 // Write your tests here
+import AppFunctional from "./AppFunctional"
+import React from 'react'
+import { render, screen, fireEvent } from "@testing-library/react"
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(false).toBe(false)
+})
+
+test('Render without errors', () => {
+  render(<AppFunctional />)
+})
+
+test('has a title', () => {
+  render(<AppFunctional />)
+
 })
